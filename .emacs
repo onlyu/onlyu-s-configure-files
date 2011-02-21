834e9cbbbb51eb56b03d957e3ce310418bd77b00
onlyu <efiish@gmail.com>
2011-02-22 00:55:19 +0800

    commit lost files

--- .emacs
+++ .emacs
@@ -81,7 +81,7 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(setq org-agenda-files (list "~/org/test.org"))
+
 
 (require 'onlyu-init)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 全局设定 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- emacs/onlyu-init.el
+++ emacs/onlyu-init.el
@@ -6,3 +6,9 @@
 
 ;; add .org file to agenda
 (setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
+
+(add-to-list 'load-path
+	     "~/emacs/yasnippet")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/emacs/yasnippet/snippets")
