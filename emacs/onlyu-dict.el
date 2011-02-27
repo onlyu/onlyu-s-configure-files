e411a83859eb2802d91fe4c51610bead1c2d2d58
onlyu <efiish@gmail.com>
2011-02-28 01:17:35 +0800

    dict

--- emacs/onlyu-dict.el
+++ emacs/onlyu-dict.el
@@ -2,9 +2,16 @@
 
 (require 'get-url-content)
 
+(defgroup onlyu-dict nil
+  "look-up word with google, thank you google!!"
+    :group 'convenience)
 (defcustom dict-url "www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=%s&sl=en&tl=zh-CN&restrict=pr,de&client=te"
+  ""
+  :group 'linum
+  :type 'sexp)
+
 (defun onlyu-find-word (word)
   "find the word in google dict, return it's means"
-  (interactive)
+  (interactive "P\nsworld: ")
   (get-url-content (format dict-url word)))
 (provide 'onlyu-dict)
