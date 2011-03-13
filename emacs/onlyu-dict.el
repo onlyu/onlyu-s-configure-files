b6117e3951f65a18eae9e2d6a32933c06e07c871
onlyu <efiish@gmail.com>
2011-03-14 00:56:03 +0800

    add ido to emacs

--- emacs/onlyu-dict.el
+++ emacs/onlyu-dict.el
@@ -1,17 +1,23 @@
 ;; look-up english word
 
 (require 'get-url-content)
+(require 'json)
 
 (defgroup onlyu-dict nil
   "look-up word with google, thank you google!!"
     :group 'convenience)
-(defcustom dict-url "www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=%s&sl=en&tl=zh-CN&restrict=pr,de&client=te"
+(defcustom dict-url "www.google.com/dictionary/json?callback=display&q=%s&sl=en&tl=zh-CN&restrict=pr,de&client=te"
   ""
-  :group 'linum
+  :group 'onlyu-dict
   :type 'sexp)
 
 (defun onlyu-find-word (word)
   "find the word in google dict, return it's means"
-  (interactive "P\nsworld: ")
-  (get-url-content (format dict-url word)))
+  (interactive "sword: ")
+  (progn
+    (setq url (format dict-url word))
+    (setq str (get-url-content url))
+    (with-output-to-temp-buffer "onlyu-dict-mode"
+      (print (replace-regexp-in-string "^.*display(\\(.*\\))" "\\1" str)))))
+      ;;(print (json-read-from-string str)))))
 (provide 'onlyu-dict)
