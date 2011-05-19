bfae8563b5984fb7935b9d46fee30c1b1f456aab
onlyu <efiish@gmail.com>
2011-05-20 01:09:47 +0800

    add some test test

--- learn/gtest-foo.cc
+++ learn/gtest-foo.cc
@@ -6,17 +6,43 @@ protected:
 	FooTest(){}
 	~FooTest(){}
 	virtual void SetUp(){
-
+	  count = 1000000;
 	}
 	virtual void TearDown(){
 
 	}	
+  static int count;
 };
 
 TEST_F(FooTest, JustForTest){
 	EXPECT_EQ(0,0);
-	EXPECT_EQ(0,1);
+	int a = 100;
+	int b = 1000;
+	ASSERT_EQ(a,b);
+	EXPECT_EQ(100,100);
 }
+
+ TEST_F(FooTest, TestPerformace){
+   int i=0;
+   for(i=0;i<count;i++){
+     EXPECT_EQ(i,i);
+   } 
+ }
+ TEST_F(FooTest, TestLoop){
+   int a=0;
+   for(int i=0;i<count;i++){
+     a++;
+   }
+   EXPECT_EQ(a, count);
+ }
+ TEST_F(FooTest, TestLoop2){
+   int a=0;
+   for(int i=0;i<count;i++){
+     a = a+1;
+     a = i*i;
+     a = i*1.0/i;
+   }
+ }
 };
 int main(int argc,char **argv)
 {
