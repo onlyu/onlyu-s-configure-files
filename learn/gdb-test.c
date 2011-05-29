fc29182a78a3c1589b550b1800268d23137915a7
onlyu <efiish@gmail.com>
2011-05-30 01:15:50 +0800

    vim plugin

--- learn/gdb-test.c
+++ learn/gdb-test.c
@@ -5,23 +5,23 @@ int foo(int val);
 
 int test(int val)
 {
-  printf("%d\n",val);
-  return val * val;
+	printf("%d\n",val);
+	return val * val;
 }
 
 int main(int argc, char *argv[])
 {
-  int i;
-  int b = 10;
-  int attach = 1;
-  for (i=0;i<b;i++){
-    int a = test(i);
-    printf("%d\n", a);
-    a = foo(a);
-    if(i=5){
-      strcpy(0, "test crash");
-    }
-    printf("%d\n", a);
-  }
-  return 0;
+	int i;
+	int b = 10;
+	int attach = 1;
+	for (i=0;i<b;i++){
+		int a = test(i);
+		printf("%d\n", a);
+		a = foo(a);
+		if(i=5){
+			strcpy(0, "test crash");
+		}
+		printf("%d\n", a);
+	}
+	return 0;
 }
