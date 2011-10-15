57c67e78614311eae32ef076b3c8af97187380e2
onlyu <efiish@gmail.com>
2011-10-15 14:10:33 +0800

    add aquamacs emacs special settings

---
+++ Library/Preferences/Aquamacs Emacs/customizations.el
@@ -0,0 +1,40 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ac-compatible-packages-regexp "^ac-")
+ '(ac-delay 0.0)
+ '(ac-expand-on-auto-complete nil)
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 213 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((fringe) (right-fringe) (left-fringe . 1) (internal-border-width . 0) (cursor-type . box) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "yellow") (foreground-color . "green") (mouse-color . "black") (tool-bar-lines . 1) (menu-bar-lines . 1) (vertical-scroll-bars))))
+ '(display-time-mode t)
+ '(ecb-layout-name "right1")
+ '(ecb-options-version "2.40")
+ '(fs-dir "F:/fs/trunk/")
+ '(global-linum-mode t)
+ '(inverse-video t)
+ '(ispell-dictionary "en")
+ '(linum-delay t)
+ '(linum-eager nil)
+ '(linum-format "%4d")
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(ns-right-alternate-modifier (quote meta))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
+ '(python-indent 8)
+ '(standard-indent 8)
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Menlo"))))
+ '(linum ((t (:background "black" :foreground "red"))))
+ '(linum-board ((t (:inherit (shadow default) :background "#888888"))))
+ '(org-mode-default ((t (:inherit outline-mode-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 180 :width normal :family "Monaco")))))
