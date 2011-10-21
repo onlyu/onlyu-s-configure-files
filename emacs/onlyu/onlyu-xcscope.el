c2e077f32cc304b8bdbb7b1a7abe4aefc20512cb
onlyu <onlyu@gmail.com>
2011-10-21 21:34:44 +0800

    cscope

--- emacs/onlyu/onlyu-xcscope.el
+++ emacs/onlyu/onlyu-xcscope.el
@@ -1,4 +1,10 @@
 
 (require 'xcscope)
+(define-key global-map "\C-cs" 'cscope-find-this-symbol)
+(define-key global-map "\C-cd" 'cscope-find-global-definition)
+(define-key global-map "\C-cf" 'cscope-find-this-file)
+(define-key global-map "\C-cc" 'cscope-find-functions-calling-this-function)
+(define-key global-map "\C-ct" 'cscope-find-this-text-string)
+(define-key global-map "\C-ci" 'cscope-find-files-including-file)
 
 (provide 'onlyu-xcscope)
