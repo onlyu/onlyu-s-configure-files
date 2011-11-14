a1f4c0fc88ae2bbe7ea96b0931cfdee3634063db
onlyu <efiish@gmail.com>
2011-11-14 20:12:13 +0800

    work.org

--- script/mount_ssh.sh
+++ script/mount_ssh.sh
@@ -2,3 +2,4 @@
 sshfs onlyu@192.168.0.8:/home/onlyu /Users/while1/sshfs/develop/
 sshfs onlyu@121.10.246.18:/home/fs/fs_run1024 /Users/while1/sshfs/1024/
 sshfs onlyu@121.10.246.15:/home/fs/fs_run1005 /Users/while1/sshfs/1005/
+sshfs onlyu@121.10.246.18:/home/onlyu /Users/while1/sshfs/1024_home/
