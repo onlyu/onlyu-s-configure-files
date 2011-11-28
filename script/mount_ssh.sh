613be06034f08f14e59e0fb8e62d89257c6a35c8
onlyu <efiish@gmail.com>
2011-11-28 11:35:13 +0800

    online.py

--- script/mount_ssh.sh
+++ script/mount_ssh.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-sshfs onlyu@192.168.0.8:/home/onlyu /Users/while1/sshfs/develop/
-sshfs onlyu@121.10.246.21:/home/fs/fs_run1024 /Users/while1/sshfs/1024/
-sshfs onlyu@121.10.246.15:/home/fs/fs_run1005 /Users/while1/sshfs/1005/
-sshfs onlyu@121.10.246.18:/home/onlyu /Users/while1/sshfs/1024_home/
+sshfs onlyu@192.168.0.8:/home/onlyu /Users/while1/sshfs/develop/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.21:/home/onlyu/ /Users/while1/sshfs/1024/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.15:/home/onlyu/ /Users/while1/sshfs/1005/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.12:/home/onlyu/ /Users/while1/sshfs/1010/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.16:/home/onlyu/ /Users/while1/sshfs/1012/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.14:/home/onlyu/ /Users/while1/sshfs/1025/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.13:/home/onlyu/ /Users/while1/sshfs/1026/ -o cache=no,follow_symlinks
+sshfs onlyu@121.10.246.17:/home/onlyu/ /Users/while1/sshfs/1027/ -o cache=no,follow_symlinks
