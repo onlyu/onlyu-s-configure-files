3e239fcdfd58515530e2ca7150b59b29fee7ba89
onlyu <efiish@gmail.com>
2011-12-21 13:32:51 +0800

    p2p test

--- learn/p2p/client.py
+++ learn/p2p/client.py
@@ -1,12 +1,20 @@
 import socket
 import time
+import sys
     
 if __name__ == '__main__':
-    
+    print sys.argv
+    if len(sys.argv) == 3:
+        ip = sys.argv[1]
+        port = int(sys.argv[2])
+    else:
+        ip = '121.10.246.15'
+        port = 8001
+        
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("", 0))
-    sock.sendto("hello server", ('121.10.246.15', 8001))
+    sock.sendto("hello server", (ip, port))
     while True:
         data, addr = sock.recvfrom(1024)
         print addr, ":", data
