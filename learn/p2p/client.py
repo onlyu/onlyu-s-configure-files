83eeb798c9f92a911320ae4f2e1a1803fd4e5603
onlyu <efiish@gmail.com>
2011-12-21 13:23:39 +0800

    p2p

--- learn/p2p/client.py
+++ learn/p2p/client.py
@@ -2,11 +2,24 @@ import socket
 import time
     
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('121.10.246.15', 8001))
-    sock.send('1')
-    print "my socket", sock.getsockname()
-    print sock.recv(1024)
-    sock.close()
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("", 0))
+    sock.sendto("hello server", ('121.10.246.15', 8001))
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print addr, ":", data
+        if addr[1] == 8001:
+            ip,ad = sock.recvfrom(1024)
+            port,ad = sock.recvfrom(1024)
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.bind(("", 0))
+            s.sendto("hello hero", (ip, int(port)))
+        else:
+            sock.sendto("hello " + str(addr), addr)
+            time.sleep(1)
+        
+
     
         
--- learn/p2p/server.py
+++ learn/p2p/server.py
@@ -1,21 +1,12 @@
 import socket
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('121.10.246.15', 8001))
-    sock.listen(5)
     while True:
-        connection, address = sock.accept()
-        print "connect from ", address
-        try:
-            connection.settimeout(5)
-            buf = connection.recv(1024)
-            if buf == "1":
-                connection.send('welcome to server')
-            else:
-                connection.send('please go out!')
-                
-        except socket.timeout:
-            print 'timeout'
-        connection.close()
+        data, addr = sock.recvfrom(1024)
+        sock.sendto("your address is "+str(addr), addr)
+        sock.sendto(addr[0], addr)
+        sock.sendto(str(addr[1]), addr)
+        print addr, ":", data
         
