4fed0a95d18f6fe979849f952f2151108ce3d29d
onlyu <efiish@gmail.com>
2011-12-21 12:01:15 +0800

    p2p

--- learn/p2p/client.py
+++ learn/p2p/client.py
@@ -3,7 +3,7 @@ import time
     
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 8001))
+    sock.connect(('192.168.12.35', 8001))
     sock.send('1')
     print sock.recv(1024)
     sock.close()
