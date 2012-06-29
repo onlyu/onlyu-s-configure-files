29dba96909f0190efa1851b76f6c564d82e62b31
onlyu <efiish@gmail.com>
2012-06-29 08:44:29 +0800

    erlang test script

--- learn/erlang/msort.erl
+++ learn/erlang/msort.erl
@@ -18,7 +18,7 @@ merge(L, []) ->
 merge([H1|T1], [H2|T2]) ->
     case H1 > H2 of
 	true ->
-	    [H2] ++ merge([H1|T1], T2);
+	    [H2 | merge([H1|T1], T2)];
 	false ->
-	    [H1] ++ merge( T1, [H2 | T2])
+	    [H1 | merge( T1, [H2 | T2])]
     end.
