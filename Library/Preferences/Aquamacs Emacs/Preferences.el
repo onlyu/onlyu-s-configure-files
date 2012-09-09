23c69427cbd9ae4c4fc1dbe7dbb222a242cd48a6
lanqing <onlyu@Freebsd_0049.qtz>
2012-09-10 03:29:14 +0800

    delete aqemacs resource

--- Library/Preferences/Aquamacs Emacs/Preferences.el
+++
@@ -1,8 +0,0 @@
-;; This is the Aquamacs Preferences file.
-;; Add Emacs-Lisp code here that should be executed whenever
-;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
-;; evaluating this file and print errors in the *Messags* buffer.
-;; Use this file in place of ~/.emacs (which is loaded as well.)
-
-(set-language-environment "chinese-gbk")
-(prefer-coding-system 'gbk)
