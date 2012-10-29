43a120b38f2ab4b6e78d07fd5af8856d9f382e47
onlyu <efiish@gmail.com>
2012-10-29 22:24:47 +0800

    golang

--- emacs/onlyu/onlyu-flymake.el
+++ emacs/onlyu/onlyu-flymake.el
@@ -90,7 +90,7 @@
 ;; flymake-simple-make-init
 
 (defun of-c-mode-common-hook ()
-  (define-key c-mode-map "\C-ce" 'of-flymake-error)
+  (define-key go-mode-map "\C-ce" 'of-flymake-error)
   (define-key pike-mode-map "\C-ce" 'of-flymake-error)
   (define-key c++-mode-map "\C-ce" 'of-flymake-error))
   
@@ -99,5 +99,6 @@
 (setq flymake-gui-warnings-enabled nil)
 (add-hook 'find-file-hooks 'flymake-find-file-hook)
 (add-hook 'c-mode-common-hook 'of-c-mode-common-hook)
+(add-hook 'go-mode-hook 'of-c-mode-common-hook)
 
 (provide 'onlyu-flymake)
