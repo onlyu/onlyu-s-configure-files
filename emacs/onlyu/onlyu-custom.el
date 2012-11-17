d97c314c39411b6209bf7d08051c75acdeb5ea1d
unknown <Administrator@GIH-D-213-0023.(none)>
2012-11-17 14:55:03 +0800

    emacs config file

--- emacs/onlyu/onlyu-custom.el
+++ emacs/onlyu/onlyu-custom.el
@@ -80,7 +80,17 @@
 (when (featurep 'Aquamacs)
   (aquamacs-toggle-full-frame))
 (when (featurep 'emacsw32)
-    (w32-maximize-frame))
+    (w32-maximize-frame)
+    (custom-set-faces
+     ;; custom-set-faces was added by Custom.
+     ;; If you edit it by hand, you could mess it up, so be careful.
+     ;; Your init file should contain only one such instance.
+     ;; If there is more than one, they won't work right.
+     '(default ((t (:inherit nil :stipple nil :background "black" :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "outline" :family "Droid Sans Mono"))))
+     '(cursor ((t (:background "yellow"))))
+     '(linum ((t (:background "black" :foreground "red"))))
+     '(linum-board ((t (:inherit (shadow default) :background "#888888"))))
+     '(org-mode-default ((t (:inherit outline-mode-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Droid Sans Mono"))))))
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
