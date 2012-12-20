24f26510266d86fd1f0cf92e0c7fa2882d000572
onlyu <efiish@gmail.com>
2012-12-20 12:11:32 +0800

    iedit config

---
+++ emacs/onlyu/onlyu-iedit.el
@@ -0,0 +1,8 @@
+(require 'iedit)
+
+(global-set-key "\C-cs" 'iedit-mode)
+
+;;(define-key global-map (kbd "C-;") 'iedit-mode)
+;;(define-key isearch-mode-map (kbd "C-;") 'iedit-mode)
+
+(provide 'onlyu-iedit)
